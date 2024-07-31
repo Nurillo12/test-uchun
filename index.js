de32e43e85a8b7fc42a06ddc1192b02229206980
Steven <isroilovnurillo2001@.com>
2024-07-31 17:35:26 +0500

    Vercel serveriga loyihani joylash, bu test uchun albatta

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const home = require("./routes/home")
+
+// Middleware
+const app = express()
+app.use(express.json())
+
+// Routes
+app.use("/home", home)
+
+// connection
+
+const port = process.env.PORT || 9001
+app.listen(port, () => console.log(`Listening to port ${port}`));
